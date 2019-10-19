dfcd1333a78a9b7dddd9c5335e53fffe504daa25
Manish Shrestha <manish.05shrestha@gmail.com>
2019-10-20 00:19:37 +0200

    Added 10 more functionalities

--- dotnet/Assignment3-TDD/Assignment3/Helpers/Validation.cs
+++ dotnet/Assignment3-TDD/Assignment3/Helpers/Validation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Cache;
 
@@ -7,10 +8,24 @@ namespace RDJTPServer.Helpers
     public class Request
     {
         public string Method;
+        public string Path;
+        public string Date;
+        public string Body;
     }
     public class Response
     {
         public string Status { get; set; }
+        public string Body { get; set; }
+    }
+
+    public enum StatusCode
+    {
+        Ok = 1,
+        Created = 2,
+        Updated = 3,
+        BadRequest = 4,
+        NotFound = 5,
+        Error = 6
     }
 
     public class Validation
@@ -18,18 +33,74 @@ namespace RDJTPServer.Helpers
         public Response ValidateRequest(Request request)
         {
             var response = new Response();
-            var allowedMethods = new[] { "create", "read", "update", "delete" };
+            var errorResponse = new List<string>();
+            var allowedMethods = new[] { "create", "read", "update", "delete", "echo", "testing" };
+
             if (request.Method == null)
             {
-                response.Status = "Missing method in header.";
+                errorResponse.Add("Missing method in header.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
             }
 
             if (!allowedMethods.Contains(request.Method))
             {
-                Console.WriteLine(request.Method);
-                response.Status = "Illegal method provided.";
+                errorResponse.Add("Illegal method request provided.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
             }
+
+            if (!allowedMethods.Contains(request.Path))
+            {
+                errorResponse.Add("Missing resource in request header.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            }
+
+            if (request.Date == null)
+            {
+                errorResponse.Add("Missing date in request header.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            }
+
+            // TODO: Research the following!!
+            if (request.Date != null && request.Date != Utilities.UnixTimestamp())
+            {
+                errorResponse.Add("Illegal date in request header.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            }
+
+            if (request.Body == null)
+            {
+                errorResponse.Add("Missing body in header.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            }
+
+            if (request.Body == null)
+            {
+                errorResponse.Add("Missing body in header.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            }
+
+            if (!Utilities.IsValidJson(request.Body))
+            {
+                errorResponse.Add("Illegal body.");
+                response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            }
+
+            // TODO: Fix the test!
+            if (request.Method != null && request.Method.Contains("echo"))
+            {
+                Console.WriteLine(request.Body);
+                response.Body = request.Body;
+            }
+
+            var pathEndpoint = request.Path.Split("/")[2];
+            //if (pathEndpoint != "categories")
+            //{
+            //    errorResponse.Add("Illegal body.");
+            //    response.Status = $"{StatusCode.BadRequest}: {string.Join(", ", errorResponse)}";
+            //}
+
             return response;
+
         }
 
     }
--- dotnet/Assignment3-TDD/Assignment3/Server.cs
+++ dotnet/Assignment3-TDD/Assignment3/Server.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using RDJTPServer.Helpers;
 
 namespace RDJTPServer
@@ -12,20 +14,29 @@ namespace RDJTPServer
         {
             Console.WriteLine("RDJTP server is starting");
             var server = new TcpListener(IPAddress.Loopback, 5000);
-            server.Start();
-
-            while (true)
+            try
             {
-                Console.WriteLine("Server started. Waiting for connection...");
-                var client = server.AcceptTcpClient();
-                Console.WriteLine("Connected.");
+                server.Start();
+                while (true)
+                {
+                    Console.WriteLine("Server started. Waiting for connection...");
+                    var client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected.");
 
-                var request = client.ReadRequest();
-                var response = new Validation().ValidateRequest(request);
-                var responseToSend = response.ToJson();
-                Console.WriteLine(responseToSend);
-                client.SendResponse(responseToSend);
+                    var request = client.ReadRequest();
+                    var response = new Validation().ValidateRequest(request);
+                    var responseToSend = response.ToJson();
+                    client.SendResponse(responseToSend);
+                }
+
+            }
+            catch (SocketException e)
+            {
+                server.Start();
+                Console.WriteLine(e);
+                throw;
             }
         }
     }
+
 }
